11a262304c4a4d59d595ba265ffb95cd22fdca6a
Carlos Diaz <carlos.diaz@outlook.fr>
2022-08-11 14:05:22 +0200

    new changes

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig){
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addWatchTarget("./src/css");
   eleventyConfig.addPassthroughCopy("./src/uploads");
+  eleventyConfig.addPassthroughCopy("./src/js");
   return {
     dir: {
       input: "src",
